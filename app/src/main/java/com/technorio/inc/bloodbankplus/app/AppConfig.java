da8b8d6bcce76ca78ad8ceb696f25408ac264340
Tenzein <tenzingsherpaaa@gmail.com>
2015-12-28 16:14:18 +0545

    error corrected

--- app/src/main/java/com/technorio/inc/bloodbankplus/app/AppConfig.java
+++ app/src/main/java/com/technorio/inc/bloodbankplus/app/AppConfig.java
@@ -5,19 +5,19 @@ public class AppConfig {
 	public static String URL_LOGIN = "http://www.bloodbankplus.com/bloodbankplus/login.php";
 
 //	// Server user register url
-//	public static String URL_REGISTER = "http://www.bloodbankplus.com/bloodbankplus/register.php";
-//
-//	public static String URL_VERIFY = "http://www.bloodbankplus.com/bloodbankplus/verification.php";
-//
-//	public static String URL_CODE_VERIFY = "http://www.bloodbankplus.com/bloodbankplus/verifynumber.php";
-//	public static String URL_STORE_APP_ID = "http://www.bloodbankplus.com/bloodbankplus/storeappid.php";
-//
+	public static String URL_REGISTER = "http://www.bloodbankplus.com/bloodbankplus/register.php";
+
+	public static String URL_VERIFY = "http://www.bloodbankplus.com/bloodbankplus/verification.php";
+
+	public static String URL_CODE_VERIFY = "http://www.bloodbankplus.com/bloodbankplus/verifynumber.php";
+	public static String URL_STORE_APP_ID = "http://www.bloodbankplus.com/bloodbankplus/storeappid.php";
+
 	// Server user register url
-	public static String URL_REGISTER = "http://192.168.0.113/bloodbankplus/register.php";
-
-	public static String URL_VERIFY = "http://192.168.0.113/bloodbankplus/verification.php";
-
-	public static String URL_CODE_VERIFY = "http://192.168.0.113/bloodbankplus/verifynumber.php";
-	public static String URL_STORE_APP_ID = "http://192.168.0.113/bloodbankplus/storeappid.php";
+//	public static String URL_REGISTER = "http://192.168.0.113/bloodbankplus/register.php";
+//
+//	public static String URL_VERIFY = "http://192.168.0.113/bloodbankplus/verification.php";
+//
+//	public static String URL_CODE_VERIFY = "http://192.168.0.113/bloodbankplus/verifynumber.php";
+//	public static String URL_STORE_APP_ID = "http://192.168.0.113/bloodbankplus/storeappid.php";
 
 }
