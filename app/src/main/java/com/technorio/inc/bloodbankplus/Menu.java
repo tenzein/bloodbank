17648f65bb616bae72120c6fc5bfd424a527138d
GTripathee <tripathee.gaurav@gmail.com>
2015-12-28 16:59:00 +0545

    SLider Changed

--- app/src/main/java/com/technorio/inc/bloodbankplus/Menu.java
+++ app/src/main/java/com/technorio/inc/bloodbankplus/Menu.java
@@ -5,17 +5,26 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
+import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Menu extends AppCompatActivity  implements View.OnClickListener{
+public class Menu extends AppCompatActivity  implements View.OnClickListener,BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener{
 
     int size = 5;
     ViewPager myPager = null;
@@ -25,6 +34,7 @@ public class Menu extends AppCompatActivity  implements View.OnClickListener{
     static TextView mDotsText[];
     private int mDotsCount;
     private LinearLayout mDotsLayout;
+    private SliderLayout mDemoSlider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,9 +58,97 @@ public class Menu extends AppCompatActivity  implements View.OnClickListener{
         btnRegister.setOnClickListener(this);
         btnMessage.setOnClickListener(this);
 
+
+
+
+        mDemoSlider = (SliderLayout)findViewById(R.id.slider);
+
+        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("BloodBank",R.drawable.banner);
+        file_maps.put("DonateBlood",R.drawable.blackb);
+        file_maps.put("SaveLife",R.drawable.redb);
+
+
+        for(String name : file_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(this);
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(file_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            mDemoSlider.addSlider(textSliderView);
+        }
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(4000);
+        mDemoSlider.addOnPageChangeListener(this);
+        //ListView l = (ListView)findViewById(R.id.transformers);
+        // l.setAdapter(new TransformerAdapter(this));
+        // l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*    @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mDemoSlider.setPresetTransformer(((TextView) view).getText().toString());
+                Toast.makeText(ViewPagerAdapter.this, ((TextView) view).getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(this, slider.getBundle().get("extra") + "", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_custom_indicator:
+                mDemoSlider.setCustomIndicator((PagerIndicator) findViewById(R.id.custom_indicator));
+                break;
+
+            case R.id.action_restore_default:
+                mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+                mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+                break;
+            case R.id.action_github:
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/daimajia/AndroidImageSlider"));
+                startActivity(browserIntent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+    @Override
+    public void onPageSelected(int position) {
+        Log.d("Slider Demo", "Page Changed: " + position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {}
 //        image slide with timer
-        ViewPagerAdapter adapter = new ViewPagerAdapter(Menu.this, size);
-        myPager = (ViewPager) findViewById(R.id.reviewpager);
+       // ViewPagerAdapter adapter = new ViewPagerAdapter(Menu.this, size);
+      /*  myPager = (ViewPager) findViewById(R.id.reviewpager);
         myPager.setAdapter(adapter);
         myPager.setCurrentItem(0);
 
@@ -72,8 +170,8 @@ public class Menu extends AppCompatActivity  implements View.OnClickListener{
                     }
                 });
             }
-        }, 500, 7000);
-    }
+        }, 500, 7000);*/
+    //}
 
     //    initialzing the button of the class
     public void initializing()
